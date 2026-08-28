4455389cfdf122c6044ce9cbf9966f6e38e9bf25
abnormaal24d <142202247+abnormaal24d@users.noreply.github.com>
2026-08-28 03:09:29 +0200

    Target Android 16 API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.gimica.mergeblast"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.gimica.mergeblast.autoplayer"
         minSdk = 29
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

dd427bcd91047168b59e98553073c58bfb031bb1
abnormaal24d <142202247+abnormaal24d@users.noreply.github.com>
2026-08-28 06:54:34 +0200

    Bump auto-resume build to 1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.gimica.mergeblast.autoplayer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

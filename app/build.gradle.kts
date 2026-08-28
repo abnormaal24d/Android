d04b17b697a2721b4883004a877ab27ff21c711f
abnormaal24d <142202247+abnormaal24d@users.noreply.github.com>
2026-08-28 07:00:28 +0200

    Bump auto-ad-close build to v1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.gimica.mergeblast.autoplayer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

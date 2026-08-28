fa1128c93f7184d2030ee53060224491c7858d39
abnormaal24d <142202247+abnormaal24d@users.noreply.github.com>
2026-08-28 08:18:43 +0200

    Bump autoplayer to v1.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.gimica.mergeblast.autoplayer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.12"
+        versionCode = 14
+        versionName = "1.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
